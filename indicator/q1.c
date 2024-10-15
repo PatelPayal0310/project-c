a6c1bf2c57f1cb0072b067e1718fb60ac177ca20
unknown <p6579113@gmail.com>
2024-10-15 17:55:35 +0530

    project

---
+++ indicator/q1.c
@@ -0,0 +1,15 @@
+//
+#include<stdio.h>
+int main(){
+	char *ptr;
+	char str[100];
+	printf("Enter any string:");
+	gets(str);
+	int i,count=0;
+	for(i=0; str[i] !='\0';i++){
+//		ptr =&str;
+//		printf("%u\n",ptr);
+		count++;
+	}
+	printf("The length of string is :%d",count);	
+}
